9119ab5159804fb81bf2b027e46488b6a5263187
Abdellah427 <abdellah.hassani2002@gmail.com>
2024-04-26 19:11:16 +0300

    home activity

--- app/src/main/java/com/example/kidwise/MainActivity.java
+++ app/src/main/java/com/example/kidwise/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.kidwise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
     }
+
 }
