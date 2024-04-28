8f54dda948347b91f8d343f715b1a6273971a94e
Abdellah427 <abdellah.hassani2002@gmail.com>
2024-04-28 15:57:40 +0300

    ANALOG CLOCK FINALLY!!!

--- app/src/main/java/com/example/kidwise/learning/ClockActivity.java
+++ app/src/main/java/com/example/kidwise/learning/ClockActivity.java
@@ -1,16 +1,73 @@
 package com.example.kidwise.learning;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import com.example.kidwise.CustomClockView;
 import com.example.kidwise.R;
+import java.util.Random;
 
 public class ClockActivity extends AppCompatActivity {
+
+    private NumberPicker hourPicker, minutePicker;
+    private Button nextButton;
+    private TextView questionText;
+    private CustomClockView customClockView;
+    private int currentHour, currentMinute;
+    private int questionCount = 0;
+    private static final int TOTAL_QUESTIONS = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
+
+        customClockView = findViewById(R.id.analog_clock);
+        nextButton = findViewById(R.id.button_next);
+        questionText = findViewById(R.id.textview_question);
+        hourPicker = findViewById(R.id.hour_picker);
+        minutePicker = findViewById(R.id.minute_picker);
+
+        // Configure the NumberPickers
+        hourPicker.setMinValue(1);
+        hourPicker.setMaxValue(12);
+
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        minutePicker.setValue(0); // Initialize picker to 0
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedHour = hourPicker.getValue();
+                int selectedMinute = minutePicker.getValue();
+                if (selectedHour == currentHour && selectedMinute == currentMinute) {
+                    questionCount++;
+                    if (questionCount < TOTAL_QUESTIONS) {
+                        generateRandomTime();
+                    } else {
+                        questionText.setText(getString(R.string.end_of_questions));
+                        nextButton.setEnabled(false);
+                    }
+                } else {
+                    questionText.setText(getString(R.string.try_again));
+                }
+            }
+        });
+
+        generateRandomTime();
+    }
+
+    private void generateRandomTime() {
+        Random random = new Random();
+        currentHour = random.nextInt(12) + 1;
+        currentMinute = random.nextInt(60); // Generate minutes from 0 to 59
+
+        customClockView.setTime(currentHour, currentMinute); // Set the time on the custom clock
+
+        questionText.setText(getString(R.string.what_time_is_it));
     }
 }
--- app/src/main/java/com/example/kidwise/redirect/LearningActivity.java
+++ app/src/main/java/com/example/kidwise/redirect/LearningActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.example.kidwise.learning.*;
 
 import com.example.kidwise.R;
@@ -32,70 +33,72 @@ public class LearningActivity extends AppCompatActivity {
         clockButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, ClockActivity.class));
+
+
+                startActivity(new Intent(getApplicationContext(), ClockActivity.class));
             }
         });
 
         seasonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, SeasonActivity.class));
+                startActivity(new Intent(getApplicationContext(), SeasonActivity.class));
             }
         });
 
         daysOfWeekButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, DaysOfWeekActivity.class));
+                startActivity(new Intent(getApplicationContext(), DaysOfWeekActivity.class));
             }
         });
 
         monthsOfYearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, MonthsOfYearActivity.class));
+                startActivity(new Intent(getApplicationContext(), MonthsOfYearActivity.class));
             }
         });
 
         digitMemoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, DigitMemoryActivity.class));
+                startActivity(new Intent(getApplicationContext(), DigitMemoryActivity.class));
             }
         });
 
         wordSpellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, WordSpellActivity.class));
+                startActivity(new Intent(getApplicationContext(), WordSpellActivity.class));
             }
         });
 
         directionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, DirectionsActivity.class));
+                startActivity(new Intent(getApplicationContext(), DirectionsActivity.class));
             }
         });
 
         multiplicationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, MultiplicationActivity.class));
+                startActivity(new Intent(getApplicationContext(), MultiplicationActivity.class));
             }
         });
 
         similarPicturesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, SimilarPicturesActivity.class));
+                startActivity(new Intent(getApplicationContext(), SimilarPicturesActivity.class));
             }
         });
 
         followObjectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LearningActivity.this, FollowObjectActivity.class));
+                startActivity(new Intent(getApplicationContext(), FollowObjectActivity.class));
             }
         });
     }
